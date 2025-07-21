711916c85b535877f9036d6fc5cba4ebfa57c8ce
Jonas <jonasbucher95@gmail.com>
2025-07-21 15:19:49 +0200

    chore(api): update endpoints to use /api/v1/

--- backend/src/main/java/com/github/jnsbdev/controller/ShiftController.java
+++ backend/src/main/java/com/github/jnsbdev/controller/ShiftController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/shift")
+@RequestMapping("/api/v1/shift")
 @RequiredArgsConstructor
 public class ShiftController {
 
